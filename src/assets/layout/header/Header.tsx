22c0e5d7d75c5c17c4bce160214f8b08556d6d57
elspirito <el_spirito@bk.ru>
2023-07-21 21:41:37 +0300

    add Header.tsx

---
+++ src/assets/layout/header/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from "styled-components";
+
+
+export const Header = () => {
+    return (
+        <StyledHeader>
+        </StyledHeader>
+    );
+};
+
+const StyledHeader = styled.div`
+  height: 300px;
+  background-color: blue;
+`
