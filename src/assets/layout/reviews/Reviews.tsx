6d75da57eac4aa2a0e160707be170e779cb42ba8
elspirito <el_spirito@bk.ru>
2023-07-24 13:40:46 +0300

    add review tabs

--- src/assets/layout/reviews/Reviews.tsx
+++ src/assets/layout/reviews/Reviews.tsx
@@ -2,12 +2,40 @@ import React from 'react';
 import styled from "styled-components";
 import {ReviewsTabs} from "./reviews-tabs/ReviewsTabs";
 import {SectionTitle} from "../../components/SectionTitle";
+import ava1 from "../../images/reviews/ava1.webp";
+import ava2 from "../../images/reviews/ava2.webp";
+import ava3 from "../../images/reviews/ava3.webp";
+import ava4 from "../../images/reviews/ava4.webp";
+import ava5 from "../../images/reviews/ava5.webp";
+
+const reviewAuthor = [
+    {
+        authorAvatar: ava1,
+        authorName: "Иванов Иван"
+    },
+    {
+        authorAvatar: ava2,
+        authorName: "Сидоров Сидор"
+    },
+    {
+        authorAvatar: ava3,
+        authorName: "Сидоров Сидор"
+    },
+    {
+        authorAvatar: ava4,
+        authorName: "Сидоров Сидор"
+    },
+    {
+        authorAvatar: ava5,
+        authorName: "Сидоров Сидор"
+    }
+];
 
 export const Reviews = () => {
     return (
         <StyledReviews>
             <SectionTitle>Отзывы клиентов</SectionTitle>
-            <ReviewsTabs/>
+            <ReviewsTabs author={reviewAuthor}/>
             <Review>
                 Балабоба демонстрирует, как с помощью нейросетей семейства YaLM можно продолжать тексты на любую тему,
                 сохраняя связность и заданный стиль. В основе Балабобы лежит первая версия YaLM, которая была
--- src/assets/layout/reviews/reviews-tabs/ReviewsTabs.tsx
+++ src/assets/layout/reviews/reviews-tabs/ReviewsTabs.tsx
@@ -1,22 +1,42 @@
 import React from 'react';
 import styled from "styled-components";
 
-export const ReviewsTabs = () => {
+
+export const ReviewsTabs = (props: { author: Array<object> }) => {
     return (
         <StyledReviewTabs>
-            <ReviewTab/>
-            <ReviewTab/>
-            <ReviewTab/>
-            <ReviewTab/>
-            <ReviewTab/>
+            {
+                props.author.map((current: any) => {
+                    return (
+                        <ReviewTab>
+                            <Avatar width={"48px"} height={"48px"} src={current.authorAvatar} alt=""/>
+                            <Author>{current.authorName}</Author>
+                        </ReviewTab>
+                    )
+                })
+            }
         </StyledReviewTabs>
     );
 };
 
-const StyledReviewTabs = styled.div `
+const StyledReviewTabs = styled.div`
   display: flex;
 `
-const ReviewTab = styled.div `
+
+const ReviewTab = styled.div`
+  background-color: #555;
+  padding: 8px 40px 8px 8px;
+  margin-right: 24px;
+  color: #fafafa;
+  border-radius: 24px;
+  display: flex;
+  gap: 8px;
+  align-items: center;
+`
+const Avatar = styled.img`
+  border-radius: 50%;
+`
+const Author = styled.span`
 
 `
 
